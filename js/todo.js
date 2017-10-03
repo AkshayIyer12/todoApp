87661ca57ed9c60240cda1776e2879a518b98f77
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-10-03 07:23:46 +0530

    Fix bug in storing desc value

--- js/todo.js
+++ js/todo.js
@@ -7,16 +7,20 @@ function onload () {
 }
 function createTask () {
   let tid = parseInt(Math.random() * (11 - 1) * 10)
-  if (data[tid] === undefined) {
-    data[tid] = {}
-    data[tid].tid = tid
-    data[tid].tname = name.value
-    data[tid].tdate = date.value
-  console.log(data)
-    
-    date[tid].td = desc.value
-  console.log(data)
-    return data
+  data[tid] = {}
+  data[tid].tid = tid
+  data[tid].tname = name.value
+  data[tid].tdate = date.value
+  data[tid].tdesc = desc.value
+  return data
+}
+function hashCode(tname) {
+  let hash = 0, i, chr;
+  if (tname.length === 0) return hash;
+  for (i = 0; i < tname.length; i++) {
+    chr   = tname.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
   }
-
+  return hash;
 }
